893adb22c9362cc41420b926e64a3505dadbf0b7
Volodymyr <volodymyr@Volodymyrs-MacBook-Pro.local>
2023-10-12 21:12:05 +0300

    complete initial setup of folders and store

---
+++ src/setup/store/models/index.ts
@@ -0,0 +1,10 @@
+import {Models} from '@rematch/core';
+// import {auth} from './auth';
+
+export interface RootModel extends Models<RootModel> {
+//   auth: typeof auth;
+}
+
+export const models: RootModel = {
+//   auth,
+};
