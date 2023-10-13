0114c7b02560af73ece8bce06038c2dc48c802f6
Volodymyr <volodymyr@Volodymyrs-MacBook-Pro.local>
2023-10-13 10:41:12 +0300

    complete initial setup of ui libriary and color schemes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
  * @format
  */
 
+require('react-native-ui-lib/config').setConfig({appScheme: 'default'});
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
---
+++ src/setup/theme.ts
@@ -0,0 +1,38 @@
+import {Schemes} from 'react-native-ui-lib';
+
+type Theme = {
+  colors: Schemes;
+};
+
+export const theme: Theme = {
+  colors: {
+    light: {
+      primary100: '#673ab7',
+      primary200: '#7a4fbf',
+      primary300: '#8c64c8',
+      primary400: '#9d79d0',
+      primary500: '#ae8fd8',
+      primary600: '#bfa5e0',
+      surface100: '#000000',
+      surface200: '#1e1e1e',
+      surface300: '#353535',
+      surface400: '#4e4e4e',
+      surface500: '#696969',
+      surface600: '#858585',
+    },
+    dark: {
+      primary100: '#673ab7',
+      primary200: '#7a4fbf',
+      primary300: '#8c64c8',
+      primary400: '#9d79d0',
+      primary500: '#ae8fd8',
+      primary600: '#bfa5e0',
+      surface100: '#000000',
+      surface200: '#1e1e1e',
+      surface300: '#353535',
+      surface400: '#4e4e4e',
+      surface500: '#696969',
+      surface600: '#858585',
+    },
+  },
+};
