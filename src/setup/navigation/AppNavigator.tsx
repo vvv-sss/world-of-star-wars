0d3b5aea97524b565fa1fa35fa059d437e976a18
Volodymyr <volodymyr@Volodymyrs-MacBook-Pro.local>
2023-10-14 13:40:08 +0300

    fix add navigator layout

--- src/setup/navigation/AppNavigator.tsx
+++ src/setup/navigation/AppNavigator.tsx
@@ -1,32 +1,44 @@
 import * as React from 'react';
+import {Colors, View} from 'react-native-ui-lib';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {FavouritesScreen, HomeScreen, SearchScreen} from '../../screens';
-import {Colors} from 'react-native-ui-lib';
+import {StyleSheet} from 'react-native';
 import {AppNavigatorParamList} from '../../types';
+import {FavouritesScreen, HomeScreen, SearchScreen} from '../../screens';
+import {FontIcon} from '../../components/atoms';
 
 const Tab = createBottomTabNavigator<AppNavigatorParamList>();
 
+type IconItem = {
+  name: string;
+  color: string;
+  size: number;
+  focused: boolean;
+};
+
 const AppNavigator: React.FC = () => {
+  const renderIcon = (data: IconItem) => {
+    const {name, color, size, focused} = data;
+
+    return (
+      <View
+        style={[
+          styles.iconContainer,
+          // eslint-disable-next-line react-native/no-inline-styles
+          {borderColor: focused ? Colors.surface600 : 'transparent'},
+        ]}>
+        <FontIcon name={name} color={color} size={size} />
+      </View>
+    );
+  };
+
   return (
     <Tab.Navigator
       screenOptions={{
         tabBarInactiveBackgroundColor: Colors.surface100,
-        tabBarActiveBackgroundColor: Colors.surface200,
-        tabBarInactiveTintColor: '#f8ca12',
-        tabBarActiveTintColor: '#ffffff',
-        tabBarIconStyle: {marginTop: 4},
-        tabBarLabelStyle: {
-          fontSize: 13,
-          color: Colors.surface600,
-          paddingBottom: 3,
-        },
+        tabBarInactiveTintColor: Colors.surface600,
+        tabBarActiveTintColor: Colors.surface600,
         tabBarStyle: {
           height: 100,
-          position: 'absolute',
-          bottom: 0,
-          left: 0,
-          right: 0,
-          zIndex: 4,
           borderTopWidth: 1,
           padding: 0,
           borderTopColor: Colors.surface600,
@@ -34,30 +46,45 @@ const AppNavigator: React.FC = () => {
         },
         headerShown: false,
         unmountOnBlur: true,
+        tabBarShowLabel: false,
       }}>
       <Tab.Screen
         name="HomeScreen"
         component={HomeScreen}
         options={{
-          tabBarLabel: 'Home',
+          tabBarIcon: ({color, size, focused}) =>
+            renderIcon({name: 'home', color, size, focused}),
         }}
       />
       <Tab.Screen
         name="SearchScreen"
         component={SearchScreen}
         options={{
-          tabBarLabel: 'Search',
+          tabBarIcon: ({color, size, focused}) =>
+            renderIcon({name: 'home', color, size, focused}),
         }}
       />
       <Tab.Screen
         name="FavouritesScreen"
         component={FavouritesScreen}
         options={{
-          tabBarLabel: 'Favourites',
+          tabBarIcon: ({color, size, focused}) =>
+            renderIcon({name: 'home', color, size, focused}),
         }}
       />
     </Tab.Navigator>
   );
 };
+
+const styles = StyleSheet.create({
+  iconContainer: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    borderWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default AppNavigator;
